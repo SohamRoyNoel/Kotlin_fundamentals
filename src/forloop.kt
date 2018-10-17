912d9d4c09089f334d2098bd8e9900a4930ea042
Pewdiepie <noelleroy720@gmail.com>
2018-10-17 20:57:56 +0530

    Loops

---
+++ src/forloop.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+    /*
+    for (i in 1..10){
+        if (i % 2 == 0)
+            println(i)
+    }
+    */
+
+    var i:Int = 1
+    while (i <= 10){
+        println(i)
+        i++
+    }
+}
---
+++ src/whileLoop.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>){
+    val x = 3
+    when(x){
+        1->{
+            println("congo! got the boob")
+        }
+        2->{
+            println("You got the pussy")
+        }
+        else->{
+            println("Bitch not found")
+            println("Value of x : ${x}")
+        }
+    }
+}
