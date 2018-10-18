f05dda3f310920e4ed499e353b692f64fd02ba6c
Pewdiepie <noelleroy720@gmail.com>
2018-10-19 01:16:17 +0530

    Inheritance_Primary_Secondary_Constructors

--- src/Inheritance_Primary_Secondary_Constructors.kt
+++ src/Inheritance_Primary_Secondary_Constructors.kt
@@ -1,19 +1,18 @@
 fun main(args: Array<String>) {
-
+    var dog = Dog12("White", "BUTT")
 }
 
 
-open class Animal1 {
-
-    var color: String = ""
-
-    fun eat() {
-        println("Eat")
+open class Animal12(var color: String) {
+    init {
+        println("Color From BASE: ${color}")
     }
 }
 
-class Dog1 : Animal1() {
-    var bread: String = ""
-
-
+// 1. Call PRIMARY Const
+// 2. SEQUENCE should be maintained :: PRIMARY Const -> 1st ;
+class Dog12(color:String, var breed: String) : Animal12(color) {
+    init {
+        println("Color DERIVED : ${color} ---> ${breed}")
+    }
 }
