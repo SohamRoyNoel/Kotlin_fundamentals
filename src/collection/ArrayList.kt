0b810b9862b622e8d23b00999460332838ecaec3
Pewdiepie <noelleroy720@gmail.com>
2018-10-24 00:41:57 +0530

    ArrayList when Immutable-listOf

---
+++ src/collection/ArrayList.kt
@@ -0,0 +1,36 @@
+package collection
+
+
+fun main(args: Array<String>){
+
+    // listOf makes a list IMMUTABLE
+    var myList = listOf<String>("Tom", "Dick", "Harry")
+
+    for (e in myList){
+        print("${e} ")
+    }
+
+    println()
+
+    for (index in 0..myList.size - 1){
+        print("${myList[index]} ")
+    }
+
+    println()
+
+    for (index in 0..myList.size - 1){
+        print("${index} ")
+    }
+
+    println()
+
+    for (index in 0..myList.size - 1){
+        print("${myList.get(index)} ")
+    }
+    /*
+            Tom Dick Harry
+            Tom Dick Harry
+            0 1 2
+            Tom Dick Harry
+    */
+}
